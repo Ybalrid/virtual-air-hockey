2a52006518ddd469687a1b2fb47910f217ea9be5
Ybalrid <ybalrid@cecmu.org>
2016-01-03 16:28:34 +0100

    prevent the player's paddle to leave his own table area

--- project/PlayerPaddleAction.cpp
+++ project/PlayerPaddleAction.cpp
@@ -6,7 +6,7 @@ using namespace Annwvyn;
 PlayerPaddleAction::PlayerPaddleAction(AnnGameObject* playerPaddle) : constructListener(), 
 	paddle(playerPaddle),
 	paddleSpeed(3.f),
-	deadzone(0.15f)
+	deadzone(0.20f)
 {
 	AnnDebug() << "PLAYER PADDLE ACTION CREATED";
 }
@@ -21,12 +21,19 @@ void PlayerPaddleAction::StickEvent(AnnStickEvent e)
 	AnnStickAxis horiz = e.getAxis(0);
 	AnnStickAxis vert = e.getAxis(1);
 
-	AnnDebug() << "Player axis : " << horiz.getAbsValue() << " & "<< vert.getAbsValue(); 
+	//AnnDebug() << "Player axis : " << horiz.getAbsValue() << " & "<< vert.getAbsValue(); 
 	
 	AnnVect3 currentVelocity(paddle->getBody()->getLinearVelocity());
 
 	//Calculate the normal velocity vector the object should have thanks to the 2 dimentional analog input
 	AnnVect3 inputVelocity(horiz.getAbsValue(), 0, vert.getAbsValue());
+	
+	if((paddle->pos().x < -0.70 && inputVelocity.x < 0) 
+		|| (paddle->pos().x > 0.70 && inputVelocity.x > 0)) inputVelocity.x = 0;
+
+	if((paddle->pos().z > 1.15 && inputVelocity.z > 0) 
+		|| (paddle->pos().z <= 0.1) && inputVelocity.z < 0) inputVelocity.z = 0;
+
 	inputVelocity.x = trim(inputVelocity.x, deadzone);
 	inputVelocity.y = trim(inputVelocity.y, deadzone);
 	
@@ -35,7 +42,7 @@ void PlayerPaddleAction::StickEvent(AnnStickEvent e)
 
 	inputVelocity *= paddleSpeed;
 	inputVelocity.y = currentVelocity.y;
-	AnnDebug() << "input velocity is : " << inputVelocity;
+	//AnnDebug() << "input velocity is : " << inputVelocity;
 
 	paddle->setLinearSpeed(inputVelocity);
 
@@ -44,4 +51,5 @@ void PlayerPaddleAction::StickEvent(AnnStickEvent e)
 
 	//Prevent the body to be "put to sleep" by the physics engine
 	paddle->getBody()->activate();
+	AnnDebug() << paddle->pos();
 }
